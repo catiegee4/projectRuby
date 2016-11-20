721a6b8e31919eb57690cce19d684c5a6c05ae67
dnahol <dalia.nahol@gmail.com>
2016-11-19 16:01:10 -0800

    first commit

---
+++ Assets/astronaut_agent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "ground")
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+
+        }
+    }
+}
